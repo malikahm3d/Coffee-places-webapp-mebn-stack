cdcf77834be8fa96af73a732b7b20869e96bb837
malik <malikon00@gmail.com>
2021-01-05 10:31:47 +0300

    added some comments

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
@@ -14,11 +13,14 @@ router.post('/register', WrapAsync(async (req, res) => {
         const user = new User({ username, email });
         const registeredUser = await User.register(user, password);
         //we split it like this so that passport handles all the hashing
-        console.dir(registeredUser);
-        req.flash('register', `Welcome to cawfeeplaces ${username}!`);
+        req.flash('success', `Welcome to cawfeeplaces ${username}!`);
+        //if succes, add flash message and redircet the user to index page
         res.redirect('/coffeeplaces');
     } catch(e) {
         req.flash('error', e.message);
+        //if error, add flash with apporpoiate messgae from the error handler
+        //and redirect to the regester page
+        //TO DO: flash the message without refreshing the page (i.e. without deleting any valid input)
         res.redirect('/register');
     }
 }));
